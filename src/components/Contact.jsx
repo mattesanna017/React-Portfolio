7712ac32e0e13fa2571b14eef05804775b6b7901
mattesanna017 <matteo.mog.sanna@gmail.com>
2022-08-12 14:09:17 +0100

    component contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,51 +1,22 @@
-import React from 'react';
-import { Link } from 'react-scroll';
+import React from 'react'
 
-
-const Footer = () => {
+const Contact = () => {
   return (
-      <div>
-        <footer className='fixed w-full bottom-0 p-4 bg-[#c9c0bb] sm:p-6'>
-            <div class="sm:flex sm:items-center sm:justify-between">
-                <span class="text-sm text-[#b22222] sm:text-center">© 2022 <a href="https://github.com/mattesanna017" class="hover:underline">Matteo Sanna</a>. All Rights Reserved.
-                </span>
-                <ul class="flex flex-wrap items-center mt-3 text-sm text-black dark:text-gray-400 sm:mt-0">
-                    <li className='hover:text-[#b22222]'>
-                        <Link to='home' smooth={true} duration={500}>
-                            Home
-                        </Link>
-                    </li>
-                    <li className='hover:text-[#b22222]'>
-                        <Link to='about' smooth={true} duration={500}>
-                        About Me
-                        </Link>
-                    </li>
-                    <li className='hover:text-[#b22222]'>
-                        <Link to='resume' smooth={true} duration={500}>
-                        Resume
-                        </Link>
-                    </li>
-                    <li className='hover:text-[#b22222]'>
-                        <Link to='projects' smooth={true} duration={500}>
-                        Projects
-                        </Link>
-                    </li>
-                    <li className='hover:text-[#b22222]'>
-                        <Link to='contact' smooth={true} duration={500}>
-                        Contact
-                        </Link>
-                    </li>
-                    <li className='hover:text-[#b22222]'>
-                        <a href="https://www.linkedin.com/in/matteo-sanna-837170106/" target='#'>LinkedIn</a>
-                    </li>
-                    <li className='hover:text-[#b22222]'>
-                        <a href="https://github.com/mattesanna017" target='#'>GitHub</a>
-                    </li>
-                </ul>
-            </div>
-        </footer>
-      </div>
+    <div name='contact' className='w-full h-screen bg-[#e5e4e2] flex justify-center items-center p-4'>
+      <form method='POST' action="https://getform.io/f/fe9de62a-5eae-44a7-a85e-bdf2dbcdaa4a" className='flex flex-col max-w-[600px] w-full'>
+          <div className='pb-8'>
+              <p className='text-4xl text-[#555d50] font-bold inline border-b-4 border-[#b22222] '>Contact</p>
+              <p className='text-[#555555] text-xl py-4'> Send a message by email: <a className='text-[#b22222]' href="mailto:atmason90@gmail.com">matteo.mog.sanna@gmail.com</a></p>
+              <p className='text-[#555555] text-xl py-4'> --- OR ---</p>
+              <p className='text-[#555555] text-xl py-4'>Fill out the form below to contact me</p>
+          </div>
+          <input className='bg-[#c9c0bb] p-2' type="text" placeholder='Name' name='name' />
+          <input className='my-4 p-2 bg-[#c9c0bb]' type="email" placeholder='Email' name='email' />
+          <textarea className='bg-[#c9c0bb] p-2' name="message" rows="10"></textarea>
+          <button className='text-black border-2 hover:bg-[#b22222] hover:border-black px-4 py-3 my-8 mx-auto items-center border-black'>Send</button>
+      </form>
+    </div>
   )
 }
 
-export default Footer
+export default Contact
